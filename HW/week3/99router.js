cffd37d50d05cc5e0084ecba13262b4aa9d2c516
al2698 <mr20166@gmail.com>
2021-12-10 15:16:26 +0800

    1516

--- HW/week3/99router.js
+++ HW/week3/99router.js
@@ -2,7 +2,7 @@ import { table99 } from "./99.js";
 console.log(`
 <html>
     <head>
-        <link rel="stylesheet" href="https://github.com/al2698/ws110a/blob/master/HW/week3/table99.css"></link>
+        <link rel="stylesheet" href="https://al2698.github.io/ws110a/HW/week3/table99.css"></link>
     </head>
     <body>
         ${table99()}
