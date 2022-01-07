a9173ef640ba947596e0de3d2e60f39034c6a644
al2698 <mr20166@gmail.com>
2022-01-07 17:04:59 +0800

    1704

--- HW/week3/99router.js
+++ HW/week3/99router.js
@@ -1,11 +1,36 @@
+import { Application, Router, send } from "https://deno.land/x/oak/mod.ts";
 import { table99 } from "./99.js";
-console.log(`
-<html>
-    <head>
-        <link rel="stylesheet" href="https://al2698.github.io/ws110a/HW/week3/table99.css"></link>
-    </head>
-    <body>
-        ${table99()}
-    </body>
-</html>
-`)
+
+
+const app = new Application();
+const router = new Router();
+router
+  .get("/", (context) => {
+    context.response.body =`
+    <html>
+      <head>
+        <link rel="stylesheet" href="table99.css">
+      </head>
+      <body style="margin: 0;">
+        <div style="background-color: ivory; width: 700px;">
+          <h1 style="text-align: center; border-bottom: 8px dotted #89B3A4">
+            九九乘法表
+          </h1>
+        ${table99()}  <!-- return table_list 放入元素 -->
+        <div>
+      </body>
+    </html>`;
+  })
+app.use(router.routes());
+app.use(router.allowedMethods());
+app.use(async (ctx) => {
+  // 1. 原始路徑(/)為空  2.send上去後會讀取到 --> /table99.css 
+  console.log('path=', ctx.request.url.pathname) 
+  await send(ctx, ctx.request.url.pathname, { // 把資料放入到伺服器 大概
+    root: Deno.cwd(),
+    index: "table99.css",
+  });
+
+});
+console.log('start at : http://127.0.0.1:8000')
+await app.listen({ port: 8000 });
